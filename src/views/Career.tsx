fe5517f226214287a316c931fce769fee24b0dee
Tom Vo <tomvothecoder@gmail.com>
2022-07-15 17:55:57 -0700

    Add bio section

--- src/views/Career.tsx
+++ src/views/Career.tsx
@@ -1,3 +1,4 @@
+import About from "components/Career/About";
 import Experience from "components/Career/Experience/Experience";
 import Hero from "components/Career/Hero";
 import "views/Career.css";
@@ -6,7 +7,37 @@ function Home() {
   return (
     <div className="home">
       <Hero />
-      <Experience />
+      <section className="section" id="experience">
+        <div className="container">
+          <h1 className="section-title has-text-centered">
+            — Where I've been —
+          </h1>
+          <Experience />
+        </div>
+      </section>
+
+      {/* <section className="section" id="skills">
+      <div className="container has-text-centered">
+        <h1 className="section-title">— Things I've Learned —</h1>
+        <Skills/>
+      </div>
+    </section> */}
+
+      {/* <section className="section" id="portfolio">
+      <div className="container has-text-centered">
+        <h1 className="section-title">— Some of my work —</h1>
+        <Portfolio/>
+      </div>
+    </section> */}
+
+      <section className="section" id="about">
+        <div className="container">
+          <h1 className="section-title has-text-centered">
+            — A little about me —
+          </h1>
+          <About />
+        </div>
+      </section>
     </div>
   );
 }
